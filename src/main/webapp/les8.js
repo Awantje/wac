98467c7a06e28bbc8baa79206c22afc9161c78f9
Bert Oostveen <bert.oostveen@gmail.com>
2017-08-30 11:19:42 +0200

    werk

--- src/main/webapp/les8.js
+++ src/main/webapp/les8.js
@@ -33,7 +33,7 @@ function editCountry(code) {
 	$("#Landcode").html("<label for=\"Landcode\" id=\"CODEE\">Landcode: "+data[0].Code+"</label>");
 	$("#ISO3").html("<label for=\"ISO3\" id=\"ISO3E\">ISO3Code: "+data[0].Iso3+"</label>");
 	$("#Naam").html("<label for=\"Naam\" id=\"NAAME\">Landnaam: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlandnaam\">"+"</textarea>");
-	$("#Editlandnaam").val(data[0].Naam);
+	$("#Editlandnaam").text(data[0].Naam);
 	console.log(data[0].Naam);
 	$("#Cont").html("<label for=\"Cont\" id=\"CONTE\">Continent:</label><textarea rows=\"1\" cols=\"50\" id=\"Editcontinent\">"+data[0].Continent+"</textarea>");
 	$("#stad").html("<label for=\"Stad\" id=\"STADE\">Stad: </label><textarea rows=\"1\" cols=\"50\" id=\"Editcapi\">"+data[0].Capital+"</textarea>");
