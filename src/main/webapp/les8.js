8ee826c7198a5a638f96576a69d722c0ee95cab8
Bert Oostveen <bert.oostveen@gmail.com>
2017-08-30 20:42:15 +0200

    doe het

--- src/main/webapp/les8.js
+++ src/main/webapp/les8.js
@@ -1,13 +1,13 @@
 loadcountries()
 function loadcountries() {
 	$.ajax({
-		url: "restservices/countries/",
-		method: "GET",
-		beforeSend: function (xhr) {
-		var token = window.sessionStorage.getItem("sessionToken");
-		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+		url : "restservices/countries/",
+		method : "GET",
+		beforeSend : function(xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
 		},
-		success: function (landen) {
+		success : function(landen) {
 			console.log(landen);
 			console.log(landen[0]);
 			console.log(landen[0].Naam);
@@ -17,61 +17,84 @@ function loadcountries() {
 				var naam = "<td>" + landen[i].Naam + "</td>";
 				var hoofdstad = "<td>" + landen[i].Capital + "</td>";
 				var regio = "<td>" + landen[i].Region + "</td>";
-				var string = "<tr id=" + landen[i].Code + ">" + naam + hoofdstad
-						+ regio + "</tr>";
+				var string = "<tr id=" + landen[i].Code + ">" + naam
+						+ hoofdstad + regio + "</tr>";
 				countries.append(string)
 			});
 			countries.append("</tbody>");
 
 		}
-		});
+	});
 
 }
 
 function editCountry(code) {
-	
-	uri = "/restservices/countries/"+code;
 	$.ajax({
-		url: uri,
-		method: "GET",
-		beforeSend: function (xhr) {
-		var token = window.sessionStorage.getItem("sessionToken");
-		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-		},
-		success: function (data) {
-		console.log("Editcountrycode");
-		console.log(data[0].Iso3);
-		console.log(data[0].Code);
-		console.log(data);
-		console.log(uri);
-	$("#landcode").html("<label for=\"Landcode\" id=\"CODEE\">Landcode: "+data[0].Code+"</label>");
-	$("#ISO3").html("<label for=\"ISO3\" id=\"ISO3E\">ISO3Code: "+data[0].Iso3+"</label>");;
-	$("#Naam").html("<label for=\"Naam\" id=\"NAAME\">Landnaam: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlandnaam\">"+"</textarea>");
-	$("#Editlandnaam").text(data[0].Naam);
-	$("#Cont").html("<label for=\"Cont\" id=\"CONTE\">Continent:</label><textarea rows=\"1\" cols=\"50\" id=\"Editcontinent\"></textarea>");
-	$("#Editcontinent").text(data[0].Continent);
-	$("#stad").html("<label for=\"Stad\" id=\"STADE\">Stad: </label><textarea rows=\"1\" cols=\"50\" id=\"Editcapi\"></textarea>");
-	$("#Editcapi").text(data[0].Capital);
-	$("#regio").html("<label for=\"regio\" id=\"REGIOE\">Regio: </label><textarea rows=\"1\" cols=\"50\" id=\"Editregion\"></textarea>");
-	$("#Editregion").text(data[0].Region);
-	$("#surface").html("<label for=\"surface\" id=\"OPPE\">Oppervlakte: </label><textarea rows=\"1\" cols=\"50\" id=\"Editsurface\"></textarea>");
-	$("#Editsurface").text(data[0].Surface);
-	$("#Population").html("<label for=\"Population\" id=\"INWE\">Inwoners: </label><textarea rows=\"1\" cols=\"50\" id=\"Editpop\"></textarea>");
-	$("#Editpop").text(data[0].Population);
-	$("#Government").html("<label for=\"Government\" id=\"OVERE\">Overheid: </label><textarea rows=\"1\" cols=\"50\" id=\"Editgov\"></textarea>");
-	$("#Editgov").text(data[0].Government);
-	$("#latitude").html("<label for=\"Latitude\" id=\"LATE\">Latitude: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlat\"></textarea>");
-	$("#Editlat").text(data[0].Lat);
-	$("#longtitude").html("<label for=\"Longtitude\" id=\"LONGE\">Longtitude: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlng\"></textarea>");
-	$("#Editlng").text(data[0].lng);
-	landcode = data[0].Code;
-	landcodeiso3 = data[0].Iso3;}	
-		})
-	$("body").on('click', 'tr', function(){
+				url : "/restservices/countries/" + code,
+				method : "GET",
+				beforeSend : function(xhr) {
+					var token = window.sessionStorage.getItem("sessionToken");
+					xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+				},
+				success : function(data) {
+					console.log("Editcountrycode");
+					console.log(data[0].Iso3);
+					console.log(data[0].Code);
+					console.log(data);
+					console.log(uri);
+					$("#landcode").html(
+							"<label for=\"Landcode\" id=\"CODEE\">Landcode: "
+									+ data[0].Code + "</label>");
+					$("#ISO3").html(
+							"<label for=\"ISO3\" id=\"ISO3E\">ISO3Code: "
+									+ data[0].Iso3 + "</label>");
+					;
+					$("#Naam")
+							.html(
+									"<label for=\"Naam\" id=\"NAAME\">Landnaam: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlandnaam\">"
+											+ "</textarea>");
+					$("#Editlandnaam").text(data[0].Naam);
+					$("#Cont")
+							.html(
+									"<label for=\"Cont\" id=\"CONTE\">Continent:</label><textarea rows=\"1\" cols=\"50\" id=\"Editcontinent\"></textarea>");
+					$("#Editcontinent").text(data[0].Continent);
+					$("#stad")
+							.html(
+									"<label for=\"Stad\" id=\"STADE\">Stad: </label><textarea rows=\"1\" cols=\"50\" id=\"Editcapi\"></textarea>");
+					$("#Editcapi").text(data[0].Capital);
+					$("#regio")
+							.html(
+									"<label for=\"regio\" id=\"REGIOE\">Regio: </label><textarea rows=\"1\" cols=\"50\" id=\"Editregion\"></textarea>");
+					$("#Editregion").text(data[0].Region);
+					$("#surface")
+							.html(
+									"<label for=\"surface\" id=\"OPPE\">Oppervlakte: </label><textarea rows=\"1\" cols=\"50\" id=\"Editsurface\"></textarea>");
+					$("#Editsurface").text(data[0].Surface);
+					$("#Population")
+							.html(
+									"<label for=\"Population\" id=\"INWE\">Inwoners: </label><textarea rows=\"1\" cols=\"50\" id=\"Editpop\"></textarea>");
+					$("#Editpop").text(data[0].Population);
+					$("#Government")
+							.html(
+									"<label for=\"Government\" id=\"OVERE\">Overheid: </label><textarea rows=\"1\" cols=\"50\" id=\"Editgov\"></textarea>");
+					$("#Editgov").text(data[0].Government);
+					$("#latitude")
+							.html(
+									"<label for=\"Latitude\" id=\"LATE\">Latitude: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlat\"></textarea>");
+					$("#Editlat").text(data[0].Lat);
+					$("#longtitude")
+							.html(
+									"<label for=\"Longtitude\" id=\"LONGE\">Longtitude: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlng\"></textarea>");
+					$("#Editlng").text(data[0].lng);
+					landcode = data[0].Code;
+					landcodeiso3 = data[0].Iso3;
+				}
+			})
+	$("body").on('click', 'tr', function() {
 		editCountry(this.id);
 	});
 }
-function nieuwcountry(){
+function nieuwcountry() {
 	console.log("nieuwcountry")
 	var codetn = $("#CODEN").val();
 	var iso3n = $("#ISO3N").val();
@@ -84,13 +107,25 @@ function nieuwcountry(){
 	var Governmentn = $("#OVERN").val();
 	var Latituden = $("#LATN").val();
 	var longtituden = $("#LONGN").val();
-	var country = {Code: codetn , Iso3 : iso3n , Naam : Naamn , Continent : Contn , Capital : Stadn , Region : Region , Surface :Surfacen, Population :Populationn, Government : Governmentn , Lat :Latituden, lng :longtituden}
+	var country = {
+		Code : codetn,
+		Iso3 : iso3n,
+		Naam : Naamn,
+		Continent : Contn,
+		Capital : Stadn,
+		Region : Region,
+		Surface : Surfacen,
+		Population : Populationn,
+		Government : Governmentn,
+		Lat : Latituden,
+		lng : longtituden
+	}
 	console.log(country);
 	console.log(codetn);
 	console.log("debug");
 	add(country);
 }
-function editcountry(){
+function editcountry() {
 	console.log("editcountry")
 	codete = landcode;
 	iso3e = landcodeiso3;
@@ -103,77 +138,90 @@ function editcountry(){
 	var Governmente = $("#Editgov").val();
 	var Latitudee = $("#Editlat").val();
 	var longtitudee = $("#Editlng").val();
-	var country = {Code: codete , Iso3 : iso3e , Naam : Naamne , Continent : Conte , Capital : Stade , Region : Regioe , Surface :Surfacee, Population :Populatione, Government : Governmente , Lat :Latitudee, lng :longtitudee}
+	var country = {
+		Code : codete,
+		Iso3 : iso3e,
+		Naam : Naamne,
+		Continent : Conte,
+		Capital : Stade,
+		Region : Regioe,
+		Surface : Surfacee,
+		Population : Populatione,
+		Government : Governmente,
+		Lat : Latitudee,
+		lng : longtitudee
+	}
 	console.log(country);
 	edit(country);
 }
-	function edit(country) {
-		console.log("editcountry");
-		var uri = "/restservices/countries/updateland";
-		data = JSON.stringify(country);
-		$.ajax(uri, {
-			beforeSend: function (xhr) {
-				var token = window.sessionStorage.getItem("sessionToken");
-				xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-			},
-			type: "put",
-			data: data,
-			success: function(response) {
-			$("#responsee").text("Country Updated!");
-			},
-			error: function(response) {
-			$("#responsee").text("Could not update country!");
-			}
-			}); 
-	}
-	function deletecountry(){
-		console.log("deletecountry");
-		var uri = "/restservices/countries/deleteland/" + landcode;
-		$.ajax(uri, {
-			beforeSend: function (xhr) {
-				var token = window.sessionStorage.getItem("sessionToken");
-				xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-			},
-		type: "delete",
-		success: function(response) {
-		$("#response").text("Country deleted!");
-		window.location.reload()
+function edit(country) {
+	console.log("editcountry");
+	data = JSON.stringify(country);
+	$.ajax({
+		url : "/restservices/countries/updateland",
+		type : "put",
+		beforeSend : function(xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
 		},
-		error: function(response) {
-		$("#response").text("Could not delete country!");
-		}
-		});
-		};
-	function add(country) {
-		console.log("addcountry");
-		data = JSON.stringify(country);
-		$.ajax({
-			url: "/restservices/countries/addland",
-			method: "POST",
-			beforeSend: function (xhr) {
-			var token = window.sessionStorage.getItem("sessionToken");
-			xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-			},
-			success: function (response) {
-				reply = JSON.stringify(response);
-				console.log(reply);
-			}
-			});
-		};
-	function getLandByCode(code) {
-		$.ajax({
-			url: "restservices/countries/"+code,
-			method: "GET",
-			beforeSend: function (xhr) {
-			var token = window.sessionStorage.getItem("sessionToken");
-			xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-			},
-			success: function (countryList) {
-			/* Handle countryList */
-			}
-			});
-	}
 
-	document.getElementById("nieuw").onclick = nieuwcountry;
-	document.getElementById("edit").onclick = editcountry;
-	document.getElementById("delete").onclick = deletecountry;
+		data : data,
+		success : function(response) {
+			$("#responsee").text("Country Updated!");
+		},
+		error : function(response) {
+			$("#responsee").text("Could not update country!");
+		}
+	});
+}
+function deletecountry() {
+	console.log("deletecountry");
+	var uri = "/restservices/countries/deleteland/" + landcode;
+	$.ajax(uri, {
+		beforeSend : function(xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+		},
+		type : "delete",
+		success : function(response) {
+			$("#response").text("Country deleted!");
+			window.location.reload()
+		},
+		error : function(response) {
+			$("#response").text("Could not delete country!");
+		}
+	});
+};
+function add(country) {
+	console.log("addcountry");
+	data = JSON.stringify(country);
+	$.ajax({
+		url : "/restservices/countries/addland",
+		method : "POST",
+		beforeSend : function(xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+		},
+		success : function(response) {
+			reply = JSON.stringify(response);
+			console.log(reply);
+		}
+	});
+};
+function getLandByCode(code) {
+	$.ajax({
+		url : "restservices/countries/" + code,
+		method : "GET",
+		beforeSend : function(xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+		},
+		success : function(countryList) {
+			/* Handle countryList */
+		}
+	});
+}
+
+document.getElementById("nieuw").onclick = nieuwcountry;
+document.getElementById("edit").onclick = editcountry;
+document.getElementById("delete").onclick = deletecountry;
