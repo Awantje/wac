d46a15fa37c1f564df0e66b07385b9cafe837bdc
Bert Oostveen <bert.oostveen@gmail.com>
2017-08-30 10:47:40 +0200

    fdjadl

--- src/main/webapp/les8.js
+++ src/main/webapp/les8.js
@@ -30,9 +30,9 @@ function editCountry(code) {
 		console.log(data);
 		console.log(uri);
 	$("#Landcode").html("<label for=\"Landcode\" id=\"CODE\">Landcode: "+data[0].Code+"</label>");
-	$("#ISO3").html("<label for=\"ISO3\" id=\"ISO3\">ISO3Code: "+data[0].Iso3+"</label>");
+	$("#ISO3").html("<label for=\"ISO3\" id=\"Iso3\">ISO3Code: "+data[0].Iso3+"</label>");
 	$("#Naam").html("<label for=\"Naam\" id=\"NAAM\">Landnaam: </label><textarea rows=\"1\" cols=\"50\" id=\"Editlandnaam\">"+"</textarea>");
-	$("#Naam").text(data[0].Naam);
+	$("#NAAM").text(data[0].Naam);
 	console.log(data[0].Naam);
 	$("#Cont").html("<label for=\"Cont\" id=\"CONT\">Continent:</label><textarea rows=\"1\" cols=\"50\" id=\"Editcontinent\">"+data[0].Continent+"</textarea>");
 	$("#stad").html("<label for=\"Stad\" id=\"STAD\">Stad: </label><textarea rows=\"1\" cols=\"50\" id=\"Editcapi\">"+data[0].Capital+"</textarea>");
@@ -72,8 +72,8 @@ function nieuwcountry(){
 }
 function editcountry(){
 	console.log("editcountry")
-	var codete = $("Landcode").val();
-	var iso3e = $("ISO3").val();
+	var codete = $("CODE").val();
+	var iso3e = $("Iso3").val();
 	var Naamne = $("#Naam").val();
 	var Conte = $("#CONT").val();
 	var Stade = $("#STAD").val();
